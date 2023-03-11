ebda9cee17e76454027667b3496983761bfd7386
GowriPriya <tadiparthi.gowri@gmail.com>
2023-03-11 03:28:15 -0600

    Collections Exercises

--- JavaCollections/src/assignments/ListHomework1.java
+++ JavaCollections/src/assignments/ListHomework1.java
@@ -11,6 +11,59 @@ The program should display a menu of options, and the user should be able to sel
 The program should continue to display the menu and allow the user to perform operations until they choose to quit.
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ListHomework1 {
+
+    public static void main(String... args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        boolean quit = true;
+        do {
+            System.out.println("1. Add an element to the list\n" +
+                    "2. Remove an element from the list\n" +
+                    "3. Find the minimum element in the list\n" +
+                    "4. Find the maximum element in the list\n" +
+                    "5. Print the contents of the list\n" +
+                    "6. Quit the program");
+            int menuOption = sc.nextInt();
+            int temp ;
+
+            switch(menuOption) {
+                case 1:
+                    System.out.println("Enter the integer element to be added:");
+                    list.add(sc.nextInt());
+                    break;
+
+                case 2:
+                    System.out.println("Enter the integer element to be removed:");
+                    list.remove((Integer)sc.nextInt());
+                    break;
+                case 3:
+                     temp = list.get(0);
+                    for(Integer i : list) {
+                       temp = i<temp? i: temp;
+                    }
+                    System.out.println("Minimum element in the list:"+ temp);
+                    break;
+                case 4:  temp = list.get(0);
+                    for(Integer i : list) {
+                        temp = i>temp? i: temp;
+                    }
+                    System.out.println("maximum element in the list:"+ temp);
+                    break;
+                case 5:
+                    System.out.println(list);
+                    break;
+                case 6:
+                    quit = false;
+                    break;
+            }
+
+        } while(quit);
+    }
 
 }
